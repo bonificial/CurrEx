25fac063b36f043019a4d01bf6258b5f4a6b1265
unknown <Bonie@DESKTOP-4OITT8D.local.lvcthealth.org>
2022-11-18 13:22:03 +0300

    Init'd Navbar

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import CurrExNavbar from "../components/Navbar";
+
+
+interface AppProps  {
+
+};
+
+function Home(props:AppProps) {
+    return (
+        <div>
+            <CurrExNavbar/>
+            Home Page</div>
+    );
+}
+
+export default Home;
