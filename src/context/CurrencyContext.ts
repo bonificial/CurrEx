c7ec667da0b23fa11d1af51c66ad8b64b91bafea
Bonie <mboniface107@gmail.com>
2022-11-19 17:40:43 +0300

    create and use context to keep track of base currency which is usable in the fetch operation

---
+++ src/context/CurrencyContext.ts
@@ -0,0 +1,10 @@
+import {createContext, Dispatch} from "react";
+
+interface contextInterface{
+ baseCurrency:string,
+ setBaseCurrency:Dispatch<any>
+}
+
+const currencyContext = createContext<contextInterface>({baseCurrency:'EUR',setBaseCurrency:()=>{}});
+export default currencyContext;
+
