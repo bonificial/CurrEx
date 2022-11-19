2714bc31708334e86e4fc713a23b57238bb53964
Bonie <mboniface107@gmail.com>
2022-11-19 23:01:46 +0300

    update router to capture params in url

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
+  createBrowserRouter,
+  Routes,
+  Route,
+  BrowserRouter,
 } from "react-router-dom";
 import Home from "./pages/Home";
+import CurrencyContext from "./context/CurrencyContext";
+import Details from "./pages/Details";
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <Home/>
-        },
-    ]);
-    return (
-
-        <React.StrictMode>
-            <RouterProvider router={router}/>
-        </React.StrictMode>
-
-    );
+  const [baseCurrency, setBaseCurrency] = useState("EUR");
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/details",
+      element: <Details />,
+    },
+  ]);
+  return (
+    <React.StrictMode>
+      <CurrencyContext.Provider value={{ baseCurrency, setBaseCurrency }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/details" element={<Details />} >
+            <Route path=":base/:dest/:amount" element={<Details />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CurrencyContext.Provider>
+    </React.StrictMode>
+  );
 }
 
 export default App;
