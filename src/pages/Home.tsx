458ac036cfd192dafee82beaecb8ce6b4442bcff
Bonie <mboniface107@gmail.com>
2022-11-19 17:40:59 +0300

    create and use context to keep track of base currency which is usable in the fetch operation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import CurrExNavbar from "../components/Navbar";
 import ConverterPanel from "../components/ConverterPanel";
+import {fetchLatestRates} from "../utils/api";
+import {notification} from "antd";
+import {sampleRatesResponse} from "../utils/contants";
+import currencyContext from "../context/CurrencyContext";
 
 interface AppProps {}
 
 function Home(props: AppProps) {
-  return (
+    const [currencies, setCurrencies] = useState<any>([]);
+
+
+    const { baseCurrency,setBaseCurrency } = useContext(currencyContext);
+
+     useEffect(() => {
+        (async function (){
+          const response = await fetchLatestRates(baseCurrency);
+
+            if(response.success){
+
+            setCurrencies(response.rates)
+            }else{
+                notification.error({
+                    message: 'An Error Occured',
+                    description:
+                       response?.message || "A server or processing error occured. We dont have additional details on this."
+                });
+
+            }
+
+        })()
+    }, []);
+ 
+    return (
     <div>
       <CurrExNavbar />
-      <ConverterPanel />
+      <ConverterPanel currencies={currencies} />
     </div>
   );
 }
