8e9d911fb4c057fe4e5ea491065dace32fe70f28
Bonie <mboniface107@gmail.com>
2022-11-20 03:13:36 +0300

    add API to fetch symbols data and timeseries data

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,16 +1,32 @@
-import {sampleRatesResponse} from "./contants";
+import {sampleRatesResponse, sampleSymbolsResponse, sampleTimeSeriesDataResponse} from "./contants";
 
 const requestHeaders = new Headers();
 requestHeaders.append("apikey", process.env.REACT_APP_FIXER_API_KEY || ""); //Will definitely return error if you have not setup a .env file with an API key from fixer
-
-export const fetchLatestRates = (baseCurrency:string) => {
-  let requestOptions: RequestInit = {
-    method: "GET",
-    headers: requestHeaders,
-    redirect: "follow",
-  };
-//return { ...sampleRatesResponse, base:baseCurrency };
- return fetch(`https://api.apilayer.com/fixer/latest?base=${baseCurrency}`, requestOptions)
+let requestOptions: RequestInit = {
+  method: "GET",
+  headers: requestHeaders,
+  redirect: "follow",
+};
+export const fetchLatestRates = (baseCurrency: string) => {
+  return { ...sampleRatesResponse, base: baseCurrency }; //Use this in dev only to disable API request and use test response values
+  return fetch(
+    `https://api.apilayer.com/fixer/latest?base=${baseCurrency}`,
+    requestOptions
+  )
     .then((response) => response.json())
     .catch((error) => console.log("error", error));
+};
+
+export const fetchSymbols = () => {
+  //  return { ...sampleSymbolsResponse }; ////Use this in dev only to disable API request and use test response values
+  return fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
+    .then((response) => response.json())
+    .catch((error) => console.log("error", error));
+};
+
+export const fetchTimeSeries = (startDate:string,endDate:string,base?:string,dest?:string) => {
+  // return { ...sampleTimeSeriesDataResponse }; ////Use this in dev only to disable API request and use test response values
+  return fetch(`https://api.apilayer.com/fixer/timeseries?start_date=${startDate}&end_date=${endDate}&base=${base}&symbols=${dest}`, requestOptions)
+      .then(response => response.json())
+      .catch(error => console.log('error', error));
 };
